c0e3c899d953eef4659e738d0bfdb7d5b17870a4
1CyBeR-J1 <marvellousojeleke@gmail.com>
2024-02-28 09:55:08 -0800

    Correction

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application """
+"""hbnb filter
+"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """
-    Display a HTML page like 6-index.html with filters loaded from DBStorage.
-
-    The filters include states and amenities, sorted by name.
-
-    Returns:
-        str: HTML content to be displayed.
-    """
-    states = storage.all(State)
-    amenities = storage.all(Amenity)
-
-    return render_template(
-        '10-hbnb_filters.html',
-        states=sorted(states.values(), key=lambda state: state.name),
-        amenities=sorted(amenities.values(), key=lambda amenity: amenity.name)
-    )
-
-
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """
-    Close the current SQLAlchemy Session after each request.
+def shutdown_session(exception=None):
+    """reload storage after each request
     """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route("/hbnb_filters", strict_slashes=False)
+def states_cities_list():
+    """pass states and cities sorted by name
+    and amenities
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    amenities = list(storage.all("Amenity").values())
+    amenities.sort(key=lambda x: x.name)
+    return render_template(
+        '10-hbnb_filters.html',
+        states=states,
+        amenities=amenities
+    )
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
