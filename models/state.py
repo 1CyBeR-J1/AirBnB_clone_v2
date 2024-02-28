d50ef92224f5e1d96e7ebd8b9c339a51533d1c4a
1CyBeR-J1 <marvellousojeleke@gmail.com>
2024-02-28 09:42:31 -0800

    modifications

--- models/state.py
+++ models/state.py
@@ -1,37 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-import models
+from models.base_model import BaseModel, Base
 
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-                "City",
-                cascade="all, delete",
-                backref="states"
-            )
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """returns the list of City instances"""
-            all_cities = storage.all('City')
-            city_list = []
-            for city in all_cities:
-                if all_cities[city].state_id == self.id:
-                    city_list.append(all_citites[city])
-            return city_list
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
